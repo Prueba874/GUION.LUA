aa43515238160ed8d21229405102464a6dd1ba79
Prueba874 <Arjota53@gmail.com>
2025-01-06 16:51:50 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 local lplr = game.Players.LocalPlayer
-local correctNames = {"FrivUpd", "fernanfloP091o", "Alejollllj"}
+local correctNames = {"FrivUpd", "fernanfloP091o", "Glo"}
 
 if table.find(correctNames, lplr.Name) then
     spawn(function()
